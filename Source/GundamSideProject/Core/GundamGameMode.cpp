38d93c0604983b9b218e4c00abb6ef83aebb8727
Tybo <tybo2018@gmail.com>
2024-12-05 22:50:03 -0800

    Implementing Arena Camera

--- Source/GundamSideProject/Core/GundamGameMode.cpp
+++ Source/GundamSideProject/Core/GundamGameMode.cpp
@@ -17,16 +17,39 @@ AGundamGameMode::AGundamGameMode()
 		DefaultPawnClass = PlayerCharacterBP.Class;
 	}
 	*/
+
+	static ConstructorHelpers::FClassFinder<AActor> CameraBP(TEXT("/Game/Blueprints/BP_ArenaCamera"));
+	if (CameraBP.Succeeded())
+	{
+		CameraClass = CameraBP.Class;
+	}
 }
 void AGundamGameMode::BeginPlay()
 {
 	Super::BeginPlay();
+	
+	if (CameraClass)
+	{
+		AActor* CameraActor = GetWorld()->SpawnActor<AActor>(CameraClass);
+		if (CameraActor)
+		{
+			APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+			if (PlayerController && CameraActor)
+			{
+				FViewTargetTransitionParams TransitionParams;
+				TransitionParams.BlendTime = 0.0f; // Instant transition
+				//PlayerController->SetViewTarget(CameraActor, TransitionParams);
+			}
+		}
+	}
+	/*
 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AEnemySpawner::StaticClass(), EnemySpawners);
 	// Debug log to verify spawners are found
 	UE_LOG(LogTemp, Log, TEXT("Found %d enemy spawners in the level."), EnemySpawners.Num());
 
 	// Spawns an enemy every 1 second
 	GetWorld()->GetTimerManager().SetTimer(SpawnTimerHandle, this, &AGundamGameMode::SpawnEnemy, 1.0f, true);
+	*/
 }
 void AGundamGameMode::Tick(float DeltaTime)
 {
@@ -42,9 +65,5 @@ void AGundamGameMode::SpawnEnemy()
 		{
 			Spawner->Spawn();
 		}
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("No Enemy Spawners found in the level!"));
 	}
 }
